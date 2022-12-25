14913098ab799eb037784a98f36a8c7c7df01b01
Yulia Fedorova <yulia1431@yandex.ru>
2022-12-25 23:16:37 +0300

    решение 54 задачи

---
+++ task54/Program.cs
@@ -0,0 +1,67 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] ArrayRowsDescending(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 1; j < inArray.GetLength(1); j++)
+        {
+            for (int m = 0; m < inArray.GetLength(1) - 1; m++)
+            {
+                if (inArray[i, m] < inArray[i, m + 1])
+                {
+                    int c = inArray[i, m];
+                    inArray[i, m] = inArray[i, m + 1];
+                    inArray[i, m + 1] = c;
+                }
+
+            }
+
+        }
+    }
+    return inArray;
+}
+
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+Console.WriteLine();
+int[,] newArray = ArrayRowsDescending(array);
+PrintArray(newArray);
